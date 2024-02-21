b72c544fb64b43af7b4b76ecab7b030402590292
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2024-02-20 21:32:18 -0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+// A simple program that prints text on the terminal 
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) { // start of the main method which means FahrenheightToCelsius is an executable class.
+        int saturdayFahrenheit;
+        int sundayFahrenheit;
+        saturdayFahrenheit = 78;
+        sundayFahrenheit = 81;
+        double saturdayCelsius = (5.0/9) * (saturdayFahrenheit - 32);
+        double sundayFahrenheit = (5.0/9) * (sundayFahrenheit - 32);
+        System.out.println("Weekend Averages");
+        System.out.println("Saturday: " + saturdayCelsius);
+        System.out.println("Sunday: " + sundayFahrenheit);
+    }
+}
+
+// Focus on the body of this method, which calculates and prints two Celsius values using two Fahrenheit temperatures. 
---
+++ Scanner.java
@@ -0,0 +1,34 @@
+// import java.util.Scanner; // before using the scanner, you need to tell Java that you want to use it. You do this by importing it at the beginning of your Java file.
+
+
+// public class ScannerExample {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Enter an integer: ");
+//         int userNumber = scanner.nextInt();
+
+//         scanner.nextLine();  // Consume the newline character left by nextInt()
+
+//         System.out.print("Enter your name: ");
+//         String userName = scanner.nextLine();
+
+//         System.out.println("You entered: " + userNumber + " and " + userName);
+
+//         scanner.close();
+//     }
+// }
+
+import java.util.Scanner;
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Fahrenheit value ");
+        int fahrenheit = input.nextInt();
+        System.out.print("Enter a day of the week: "); 
+        String day = input.next();
+        double celsius = (5.0/9) * (fahrenheit - 32);
+        System.out.println(day + " Fahrenheit: " + fahrenheit);
+        System.out.println(day + "Celsius: " + celsius);
+    }
+}
